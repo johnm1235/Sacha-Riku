c9916e4119280936c395a7808d7a3c458a849440
AdrianNAT00 <adrian.abad@est.ucacue.edu.ec>
2024-09-23 21:34:27 -0500

    Inventario e Ingredientes anadidos

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     float moveX = 0;
     float moveZ = 0;
+    public List<GameObject> plantas = new List<GameObject>();
+    public List<GameObject> piedras = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
